c7910414de92f3393f082407c6e82dfe7d4b29ea
Mondo192 <31599023+mondo192@users.noreply.github.com>
2021-09-04 12:27:18 +0100

    :construction:

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export const App: React.FC = () => {
+function App() {
   return (
-    <React.Fragment>
-      <h1>Webpack React App</h1>
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
+
+export default App;
